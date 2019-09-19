ec5795dfff196f2ef00f3676d41215fd17c3d931
Mathis Lécrivain <matlecr35@gmail.com>
2019-09-19 09:41:20 +0200

    add automaton to common files
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ raspberrypi/common/automaton.py
@@ -0,0 +1,26 @@
+from robots.get_robot_name import *
+
+
+class Automaton:
+    YELLOW = 0
+    PURPLE = 1
+    UNDEFINED = -1
+
+    def __init__(self):
+        pass
+
+    def set_side(self, side):
+        pass
+
+    def set_position(self):
+        pass
+
+    def run(self):
+        pass
+
+    def positioning(self):
+        pass
+
+    def start_preparation(self):
+        from robots.buttons_manager import ButtonsManager
+        ButtonsManager(self).begin()
