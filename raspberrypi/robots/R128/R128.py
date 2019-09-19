a63e6827a6cf6ffde9015708c8cc92bfb5786d07
Mathis Lécrivain <matlecr35@gmail.com>
2019-09-19 10:38:54 +0200

    add basic bornibus/128 main code structure
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ raspberrypi/robots/R128/R128.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from robots.bornibus.setup_bornibus import *
+from common.automaton import Automaton
+from managers.sensors_manager import *
+import traceback
+from managers.wheeledbase_manager import PositionUnreachable
+
+
+COLOR = Automaton.YELLOW
+COLOR = Automaton.PURPLE
+PREPARATION = False
+
+
+class R128(Automaton):
+
+    def __init__(self):
+        Automaton.__init__(self)
+        pass
+
+    def set_side(self, side):
+        pass
+
+    def set_position(self):
+        pass
+
+    def positioning(self):
+        pass
+
+    def stop_match(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    if PREPARATION:
+        R128().start_preparation()
+    else:
+        auto = R128()
+        auto.set_side(COLOR)
+        init_robot()
+        auto.set_position()
+        print("ready")
+        input()
+        auto.run()
