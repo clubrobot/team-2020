e296ce635bf485560f8b2b0cf474c49c84e605a9
Mathis Lécrivain <matlecr35@gmail.com>
2019-09-19 10:39:15 +0200

    bornibus main code structure
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ raspberrypi/robots/bornibus/setup_bornibus.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setups.setup_serialtalks import *
+from setups.setup_wheeledbase import *
+from setups.setup_roadmap import *
+from setups.setup_display import *
+#from setups.setup_sensors import *
+#from setups.setup_beacons import *
+
+
+def init_robot():
+    pass
+
+
+if __name__ == "__main__":
+    init_robot()
