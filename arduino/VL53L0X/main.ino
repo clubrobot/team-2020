646a1760ed748297f6e005c27201fb97fc188953
Mathis Lécrivain <matlecr35@gmail.com>
2019-10-20 11:05:08 +0200

    add VL53L0X arduino testu
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ arduino/VL53L0X/main.ino
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "../common/VL53L0X.h"
+
+VL53L0X s1 = VL53L0X(0X44, 11);
+VL53L0X s2 = VL53L0X(0X45, 6);
+
+long time = 0;
+
+void setup()
+{
+	pinMode(7, OUTPUT);
+	digitalWrite(7, HIGH);
+
+	Wire.begin();
+
+	Serial.begin(115200);
+
+	s1.shutdown();
+	s2.shutdown();
+
+	if (!s1.begin())
+	{
+		Serial.println("failedToBoot");
+	}
+	if (!s2.begin())
+	{
+		Serial.println("failedToBoot");
+	}
+
+	s1.setTimeout(30);
+	s2.setTimeout(30);
+
+	s1.startContinuous();
+	s2.startContinuous();
+}
+
+void loop()
+{
+	time = micros();
+	s1.readRangeContinuousMillimeters();
+	s2.readRangeContinuousMillimeters();
+	Serial.println((micros() - time));
+}
