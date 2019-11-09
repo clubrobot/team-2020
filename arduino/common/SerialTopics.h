8f86526bce1b54b903ccc6c932db79002c5337e3
Mathis Lécrivain <matlecr35@gmail.com>
2019-11-09 17:12:17 +0100

    add SerialTopics V1 working version
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ arduino/common/SerialTopics.h
@@ -0,0 +1,49 @@
+#ifndef __SERIALTOPICS_H__
+#define __SERIALTOPICS_H__
+
+#include <Arduino.h>
+#include "SerialTalks.h"
+
+#define SERIALTOPICS_DEFAULT_TIMING 100 //ms
+
+#define SERIALTOPICS_MAX_OPCODE 5
+
+#define START_SUBSCRIPTION_OPCODE 0X07
+#define STOP_SUBSCRIPTION_OPCODE 0X08
+#define GET_SUBSCRIPTION_STATE_OPCODE 0X09
+
+class SerialTopics
+{
+public:
+    typedef void (*Subscription)(Serializer &output);
+
+    typedef struct
+    {
+        /* data */
+        Subscription func;
+        long timestep;
+        long lasttime;
+        bool enable;
+    } subscription_t;
+
+    void begin(SerialTalks &talks);
+    void bind(byte opcode, Subscription instruction);
+    bool execute();
+
+    subscription_t *getSubscriptions() { return _subscriptions; }
+
+private:
+    SerialTalks *_talks;
+
+    subscription_t _subscriptions[SERIALTOPICS_MAX_OPCODE]; /*!< Listes des souscriptions enregistrées avec un OPCode associé.*/
+
+    static void START_SUBSCRIPTION(SerialTalks &talks, Deserializer &input, Serializer &output);
+    static void STOP_SUBSCRIPTION(SerialTalks &talks, Deserializer &input, Serializer &output);
+    static void GET_SUBSCRIPTION_STATE(SerialTalks &talks, Deserializer &input, Serializer &output);
+
+    static void DEFAULT_HANDLER(Serializer &output);
+};
+
+extern SerialTopics topics;
+
+#endif // __SERIALTOPICS_H__
---
+++ esp32/common/SerialTopics/SerialTopics.cpp
@@ -0,0 +1,97 @@
+#include "SerialTopics.h"
+#include "Clock.h"
+
+// Global instance
+
+SerialTopics topics;
+
+void SerialTopics::START_SUBSCRIPTION(SerialTalks &talks, Deserializer &input, Serializer &output)
+{
+    byte opcode = input.read<byte>();
+    long timestep = input.read<long>();
+
+    if (opcode < SERIALTOPICS_MAX_OPCODE)
+    {
+        topics.getSubscriptions()[opcode].timestep = timestep;
+        topics.getSubscriptions()[opcode].lasttime = 0;
+        topics.getSubscriptions()[opcode].enable = true;
+        output.write<bool>(true);
+    }
+    else
+    {
+        output.write<bool>(false);
+    }
+}
+
+void SerialTopics::STOP_SUBSCRIPTION(SerialTalks &talks, Deserializer &input, Serializer &output)
+{
+    byte opcode = input.read<byte>();
+
+    if (opcode < SERIALTOPICS_MAX_OPCODE)
+    {
+        topics.getSubscriptions()[opcode].enable = false;
+        output.write<bool>(true);
+    }
+    else
+    {
+        output.write<bool>(false);
+    }
+}
+
+void SerialTopics::GET_SUBSCRIPTION_STATE(SerialTalks &talks, Deserializer &input, Serializer &output)
+{
+    byte opcode = input.read<byte>();
+
+    if (opcode < SERIALTOPICS_MAX_OPCODE)
+    {
+        output.write<long>(topics.getSubscriptions()[opcode].timestep);
+        output.write<bool>(topics.getSubscriptions()[opcode].enable);
+    }
+    else
+    {
+        output.write<long>(-1);
+        output.write<bool>(false);
+    }
+}
+
+void SerialTopics::DEFAULT_HANDLER(Serializer &output)
+{
+}
+
+void SerialTopics::begin(SerialTalks &talks)
+{
+    _talks = &talks;
+
+    _talks->bind(START_SUBSCRIPTION_OPCODE, START_SUBSCRIPTION);
+    _talks->bind(STOP_SUBSCRIPTION_OPCODE, STOP_SUBSCRIPTION);
+    _talks->bind(GET_SUBSCRIPTION_STATE_OPCODE, GET_SUBSCRIPTION_STATE);
+
+    for (int i = 0; i < SERIALTOPICS_MAX_OPCODE; i++)
+    {
+        _subscriptions[i].timestep = SERIALTOPICS_DEFAULT_TIMING;
+        _subscriptions[i].enable = false;
+        _subscriptions[i].func = DEFAULT_HANDLER;
+    }
+}
+
+void SerialTopics::bind(byte opcode, Subscription instruction)
+{
+    if (opcode < SERIALTOPICS_MAX_OPCODE)
+        _subscriptions[opcode].func = instruction;
+}
+
+bool SerialTopics::execute()
+{
+    long currentTime = millis();
+    for (int i = 0; i < SERIALTOPICS_MAX_OPCODE; i++)
+    {
+        if (_subscriptions[i].enable && currentTime - _subscriptions[i].lasttime > _subscriptions[i].timestep)
+        {
+            Serializer ser = _talks->getSerializer();
+            _subscriptions[i].func(ser);
+            _talks->send(i, ser);
+            _subscriptions[i].lasttime = currentTime;
+        }
+    }
+    return true;
+}
