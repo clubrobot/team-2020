5c401fe492d8f9f5a752e941e76d9d1195652b17
Mathis Lécrivain <matlecr35@gmail.com>
2019-11-23 18:01:15 +0100

    thread sample
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/logs/sample.py
+++ raspberrypi/logs/sample.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from logs.log_manager import *
+import threading
+
+
+def thread_wheeledbase():
+    loggerWheeledbase = log.getlogger(
+        'Wheeledbase', exec_param=Logger.BOTH, level_disp=Logger.CRITICAL)
+    loggerWheeledbase("b", level=Logger.CRITICAL)
+    sleep(2)
+    loggerWheeledbase("c", level=Logger.ERROR)
+    sleep(2)
+    loggerWheeledbase("d", level=Logger.WARNING)
+    loggerWheeledbase("e", level=Logger.INFO)
+    loggerWheeledbase("f", level=Logger.DEBUG)
+
+
+def thread_sensors():
+    loggerSensors = log.getlogger(
+        'Sensors', exec_param=Logger.BOTH, level_disp=Logger.CRITICAL)
+
+    loggerSensors("b", level=Logger.CRITICAL)
+    loggerSensors("c", level=Logger.ERROR)
+    loggerSensors("d", level=Logger.WARNING)
+    sleep(2)
+    loggerSensors("e", level=Logger.INFO)
+    loggerSensors("f", level=Logger.DEBUG)
+
 
 if __name__ == "__main__":
 
     log = LogManager()
     log.start()
-    loggerWheeledbase = log.getlogger('Wheeledbase', exec_param=Logger.BOTH)
-    loggerSensors = log.getlogger('Sensors')
 
-    loggerWheeledbase("b", level=Logger.CRITICAL)
-    loggerWheeledbase("c", level=Logger.ERROR)
-    loggerWheeledbase("d", level=Logger.WARNING)
-    loggerWheeledbase("e", level=Logger.INFO)
-    loggerWheeledbase("f", level=Logger.DEBUG)
+    x1 = threading.Thread(target=thread_wheeledbase)
+    x2 = threading.Thread(target=thread_sensors)
 
-    loggerSensors("b", level=Logger.CRITICAL)
-    loggerSensors("c", level=Logger.ERROR)
-    loggerSensors("d", level=Logger.WARNING)
-    loggerSensors("e", level=Logger.INFO)
-    loggerSensors("f", level=Logger.DEBUG)
+    x1.start()
+    x2.start()
+
+    x1.join()
+    x2.join()
 
     sleep(1)
     log.terminate()
