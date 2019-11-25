c7de59673bc6fa9e61d3c7f6ca7cb9fe9bc78c3d
Mathis Lécrivain <matlecr35@gmail.com>
2019-11-25 15:53:10 +0100

    handle case when the log manager is not running
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/logs/utils/logger.py
+++ raspberrypi/logs/utils/logger.py
@@ -37,8 +37,11 @@ class Logger:
 
         self.exec_param = exec_param
         self.level_disp = level_disp
-        self.parent.send(LogCommand(self.parent.INIT, LogInit(
-            self.name, self.exec_param, self.level_disp)))
+        if self.parent.is_alive():
+            self.parent.send(LogCommand(self.parent.INIT, LogInit(
+                self.name, self.exec_param, self.level_disp)))
+        else:
+            raise RuntimeWarning('LogManager not running')
 
     # call write
     def __call__(self, *args, **kwargs):
@@ -46,10 +49,23 @@ class Logger:
 
     # send write command with desired message to the server
     def write(self, *args, **kwargs):
-        t = str("[{0:.3g}]".format(time() - self.parent.initial_time))
-        if 'level' in kwargs:
-            self.parent.send(LogCommand(self.parent.WRITE_LOG,
+        if self.parent.is_alive():
+            t = str("[{0:.3g}]".format(time() - self.parent.initial_time))
+            if 'level' in kwargs:
+                self.parent.send(LogCommand(self.parent.WRITE_LOG,
                                         LogMsg(t, kwargs.pop('level'), self.name, args, kwargs)))
-        else:
-            self.parent.send(LogCommand(self.parent.WRITE_LOG,
+            else:
+                self.parent.send(LogCommand(self.parent.WRITE_LOG,
                                         LogMsg(t, self.INFO, self.name, args, kwargs)))
+        else:
+            pass #TODO : Find a better way to handle this case
+
+
+class FakeLogger:
+    # call write
+    def __call__(self, *args, **kwargs):
+        pass
+
+    # send write command with desired message to the server
+    def write(self, *args, **kwargs):
+        pass
