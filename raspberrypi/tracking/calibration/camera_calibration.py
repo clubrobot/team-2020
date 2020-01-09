79930142fe85708be91083bd11b3ba614c3fea71
Mathis Lécrivain <matlecr35@gmail.com>
2020-01-09 16:25:44 +0100

    add 3d axis on chess boars to check if claibration successful
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/tracking/calibration/camera_calibration.py
+++ raspberrypi/tracking/calibration/camera_calibration.py
@@ -49,3 +49,38 @@ cv_file.write("camera_matrix", mtx)
 cv_file.write("dist_coeff", dist)
 # note you *release* you don't close() a FileStorage object
 cv_file.release()
+
+# ---------- Draw the axis on chessboard to check calibration -----------------
+# Function to draw the axis
+
+
+def draw(img, corners, imgpts):
+    corner = tuple(corners[0].ravel())
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+    return img
+
+
+axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+    if ret == True:
+        corners2 = cv2.cornerSubPix(
+            gray, corners, (11, 11), (-1, -1), criteria)
+
+        # Find the rotation and translation vectors.
+        _, rvecs, tvecs, inliers = cv2.solvePnPRansac(
+            objp, corners2, mtx, dist)
+        # project 3D points to image plane
+        imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+
+        img = draw(img, corners2, imgpts)
+        cv2.imshow('img', img)
+        cv2.waitKey(2000)
+
+
+cv2.destroyAllWindows()
