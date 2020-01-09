a816ba9deb133868bca5d4e12c97c8053eecadf9
Mathis Lécrivain <matlecr35@gmail.com>
2020-01-09 17:10:09 +0100

    get postion from markers
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/tracking/libs/markers.py
+++ raspberrypi/tracking/libs/markers.py
@@ -6,62 +6,58 @@ from collections import namedtuple
 from logs.log_manager import *
 
 Markers = namedtuple('Markers', ['corners', 'ids', 'rejectedImgPoints'])
-
-
-class MakersDisplay():
-    def __init__(self, exec_param=Logger.SHOW, log_level=INFO):
-        self.logger = LogManager().getlogger(
-            self.__class__.__name__, exec_param, log_level)
-
-        self.logger(INFO, 'MarkersDetection Initialisation Success !')
-
-    def draw(self, image, markers):
-        if markers is not None:
-            if(markers.ids is not None and len(markers.ids) > 0):
-                self.logger(INFO, 'Found', len(markers.ids),
-                            'Markers | ids : ', markers.ids)
-
-            self.logger(DEBUG, 'corners : ', markers.corners,
-                        'rejectedImgPoints : ', markers.rejectedImgPoints)
-            # Draw detected maker in green
-            aruco.drawDetectedMarkers(image, markers.corners, markers.ids)
-            # Draw rejected maker in red
-            aruco.drawDetectedMarkers(
-                image, markers.rejectedImgPoints, borderColor=(100, 0, 240))
-
-            # Show image result in real time
-            cv2.imshow('result', image)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                return False
-        return True
+PosSingleMarker = namedtuple(
+    'PosSingleMarker', ['rvecs', 'tvecs', 'objPoints'])
 
 
 class MarkersDetector():
-    def __init__(self, display=False, exec_param=Logger.SHOW, log_level=INFO):
+    def __init__(self, dictionnary=aruco.DICT_4X4_100, exec_param=Logger.SHOW, log_level=INFO):
         self.logger = LogManager().getlogger(
             self.__class__.__name__, exec_param, log_level)
 
         self.logger(INFO, 'MarkersDetector Initialisation Success !')
 
         # Get aruco dict
-        self.dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
+        self.dict = aruco.Dictionary_get(dictionnary)
 
         # Get detection parameters
         self.parameters = aruco.DetectorParameters_create()
-
-        if display:
-            self.display = MakersDisplay(exec_param, log_level)
-        else:
-            self.display = None
+        self.parameters.adaptiveThreshConstant = 10
 
     def getMarkers(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        markers = Markers(
-            *aruco.detectMarkers(gray, self.dict, parameters=self.parameters))
+        return Markers(*aruco.detectMarkers(gray, self.dict, parameters=self.parameters))
 
-        if self.display is not None:
-            self.display.draw(gray, markers)
-        return markers
+
+class MarkersPositioning():
+    def __init__(self, exec_param=Logger.SHOW, log_level=INFO):
+        self.logger = LogManager().getlogger(
+            self.__class__.__name__, exec_param, log_level)
+
+        # File storage in OpenCV
+        cv_file = cv2.FileStorage("../calibration.yaml", cv2.FILE_STORAGE_READ)
+
+        # Note : we also have to specify the type
+        # to retrieve otherwise we only get a 'None'
+        # FileNode object back instead of a matrix
+        self.camera_matrix = cv_file.getNode("camera_matrix").mat()
+        self.dist_matrix = cv_file.getNode("dist_coeff").mat()
+
+        cv_file.release()
+
+        self.logger(INFO, 'camera_matrix : ', self.camera_matrix.tolist())
+        self.logger(INFO, 'dist_matrix : ', self.dist_matrix.tolist())
+
+        cv_file.release()
+
+        self.logger(INFO, 'MarkersPositioning Initialisation Success !')
+
+    def getPositionsFromMarkers(self, markers):
+        # estimate pose of each marker and return the values
+        # rvet and tvec-different from camera coefficients
+        return PosSingleMarker(*aruco.estimatePoseSingleMarkers(
+            markers.corners, 0.05, self.camera_matrix, self.dist_matrix))
+        # (rvec-tvec).any() # get rid of that nasty numpy value array error
 
 
 class MarkersPosEstimator():
