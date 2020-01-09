834e45c5fa9be4d966485e77db7d5f54723cf70c
Mathis Lécrivain <matlecr35@gmail.com>
2020-01-09 17:15:02 +0100

    clean markers lib
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/tracking/libs/markers.py
+++ raspberrypi/tracking/libs/markers.py
@@ -11,6 +11,10 @@ PosSingleMarker = namedtuple(
 
 
 class MarkersDetector():
+    """
+        This class purpose is to detect aruco markers on frame
+    """
+
     def __init__(self, dictionnary=aruco.DICT_4X4_100, exec_param=Logger.SHOW, log_level=INFO):
         self.logger = LogManager().getlogger(
             self.__class__.__name__, exec_param, log_level)
@@ -25,11 +29,18 @@ class MarkersDetector():
         self.parameters.adaptiveThreshConstant = 10
 
     def getMarkers(self, image):
+        """
+            Return Markers tuple for a given frame
+        """
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return Markers(*aruco.detectMarkers(gray, self.dict, parameters=self.parameters))
 
 
 class MarkersPositioning():
+    """
+        This class purpose is to give aruco marker postion from previously identified markers on frame
+    """
+
     def __init__(self, exec_param=Logger.SHOW, log_level=INFO):
         self.logger = LogManager().getlogger(
             self.__class__.__name__, exec_param, log_level)
@@ -37,38 +48,24 @@ class MarkersPositioning():
         # File storage in OpenCV
         cv_file = cv2.FileStorage("../calibration.yaml", cv2.FILE_STORAGE_READ)
 
-        # Note : we also have to specify the type
-        # to retrieve otherwise we only get a 'None'
-        # FileNode object back instead of a matrix
         self.camera_matrix = cv_file.getNode("camera_matrix").mat()
         self.dist_matrix = cv_file.getNode("dist_coeff").mat()
 
+        # release file
         cv_file.release()
 
+        # show camera calibration constants
         self.logger(INFO, 'camera_matrix : ', self.camera_matrix.tolist())
         self.logger(INFO, 'dist_matrix : ', self.dist_matrix.tolist())
-
-        cv_file.release()
 
         self.logger(INFO, 'MarkersPositioning Initialisation Success !')
 
     def getPositionsFromMarkers(self, markers):
+        """
+            Return PosSingleMarker tuple for a given markers
+        """
         # estimate pose of each marker and return the values
         # rvet and tvec-different from camera coefficients
         return PosSingleMarker(*aruco.estimatePoseSingleMarkers(
             markers.corners, 0.05, self.camera_matrix, self.dist_matrix))
         # (rvec-tvec).any() # get rid of that nasty numpy value array error
-
-
-class MarkersPosEstimator():
-    def __init__(self, exec_param=Logger.SHOW, log_level=INFO):
-        self.logger = LogManager().getlogger(
-            self.__class__.__name__, exec_param, log_level)
-
-        self.cameraMatrix = None
-        self.distCoefs = None
-
-        self.rvec = None
-        self.tvec = None
-
-        self.logger(INFO, 'MarkersPosEstimator Initialisation Success !')
