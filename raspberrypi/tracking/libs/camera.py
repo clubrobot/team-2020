69e4da08be60389f526242727ebe90713050368b
Mathis Lécrivain <matlecr35@gmail.com>
2020-01-19 18:27:21 +0100

    Provide some docstring on camera class
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/tracking/libs/camera.py
+++ raspberrypi/tracking/libs/camera.py
@@ -4,9 +4,16 @@ import imutils
 
 from logs.log_manager import *
 
-
 class Camera(VideoStream):
+    """
+        The purpose of this class is to provide a camera abstraction layer to handle
+        all camera types including picamera.
+        It also compute the FPS rate.
+    """
     def __init__(self, src=0, usePiCamera=False, resolution=(320, 240), framerate=32, exec_param=Logger.SHOW, log_level=INFO):
+        """
+            Init camera videostream and FPS class.
+        """
         VideoStream.__init__(self, src, usePiCamera, resolution, framerate)
         self.logger = LogManager().getlogger(
             self.__class__.__name__, exec_param, log_level)
@@ -15,10 +22,16 @@ class Camera(VideoStream):
         self.logger(INFO, 'Camera Initialisation Success !')
 
     def start(self):
+        """
+            Strating camera process
+        """
         self.logger(INFO, 'Starting Camera !')
         super().start()
 
     def read(self, width=None):
+        """
+            Read one frame and resize it if desired
+        """
         image = super().read()
         if width is not None:
             image = imutils.resize(image, width=width)
@@ -26,6 +39,10 @@ class Camera(VideoStream):
         return image.copy()
 
     def stop(self):
+        """
+            Stopping camera process.
+            Show elapsed time and FPS approximation.
+        """
         super().stop()
         self.fps.stop()
         self.logger(INFO, "elasped time: {:.2f}".format(self.fps.elapsed()))
