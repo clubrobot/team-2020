a1c489c19d117df01f07410fa91c74542a96a605
mlecriva <matlecr35@gmail.com>
2020-02-13 17:23:50 +0100

    server IHM

---
+++ raspberrypi/beacons/server_ihm.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from time import sleep
+class ServerIHM():
+    def __init__(self):
+       self.switch1 = None
+       self.switch2 = None
+       self.switch3 = None
+       self.switch4 = None
+
+       self.screen  = None
