e008d8730ded91175869b24498c57fc44bedc54b
mlecriva <matlecr35@gmail.com>
2020-02-21 17:35:41 +0100

    new sensor listener

--- raspberrypi/listeners/sensor_listener.py
+++ raspberrypi/listeners/sensor_listener.py
@@ -1,31 +1,66 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-
 from threading import Thread, Event
-from common.sync_flag_signal import Signal
 from time import sleep
 from math import hypot
 
+from common.sync_flag_signal import Signal, Flag
+
 
 class SensorListener(Thread):
-    def __init__(self, getter, timestep=0.1, threshold=220):
+
+    FRONT = 0
+    LEFT  = 1
+    RIGHT = 2
+
+    def __init__(self, front_getter, left_getter, right_getter, timestep=0.1, threshold=220):
         Thread.__init__(self)
         self.daemon = True
-        self.signal = Signal()
-        self.getter = getter
+
+        # Sensors list
+        self.sensors_list = [LEFT, FRONT, RIGHT]
+
+        # Signals for each sensors
+        self.__setattr__("signal"+str(self.FRONT) , Signal())
+        self.__setattr__("signal"+str(self.LEFT)  , Signal())
+        self.__setattr__("signal"+str(self.RIGHT) , Signal())
+
+        # Sensors getter
+        self.__setattr__("getter"+str(self.FRONT) , front_getter)
+        self.__setattr__("getter"+str(self.LEFT)  , left_getter)
+        self.__setattr__("getter"+str(self.RIGHT) , left_getter)
+
+        # Timestep
         self.timestep = timestep
+
+        # Stopping event
         self.stop = Event()
+
+        # Position threshold
         self.threshold = threshold
-        self.error = 0
+
+        # Atomatically start
         self.start()
+
+    def bind(self, idx, func):
+        self.__setattr__("flag"+str(idx), Flag(func))
+        self.__getattribute__("flag"+str(idx)).bind(self.__getattribute__("signal"+str(idx)))
+
+    def _handle_sensor(self, idx):
+        try:
+            a, b = self.__getattribute__("getter"+str(idx))
+        except TimeoutError:
+            pass
+
+        if a < self.threshold or b < self.threshold:
+            self.__getattribute__("signal"+str(idx)).ping()
+
 
     def run(self):
         while not self.stop.is_set():
+            # Handle each sensors
+            for sensor in self.sensors_list:
+                self._handle_sensor(sensor)
+
             sleep(self.timestep)
-            try:
-                left, right = self.getter()
-            except TimeoutError:
-                continue
-            if left < self.threshold or right < self.threshold:
-                self.signal.ping()
