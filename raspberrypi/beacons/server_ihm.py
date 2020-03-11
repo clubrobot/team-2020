11644284b7dbb2ac9443406839269c3ae6fc2110
mlecriva <matlecr35@gmail.com>
2020-03-11 14:19:51 +0100

    ihm improvements

--- raspberrypi/beacons/server_ihm.py
+++ raspberrypi/beacons/server_ihm.py
@@ -2,15 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-
 import Adafruit_SSD1306   # This is the driver chip for the Adafruit PiOLED
+import RPi.GPIO as GPIO
 
 from PIL import Image, ImageDraw, ImageFont
 from time import sleep
-import RPi.GPIO as GPIO
 
 from common.parallel import Thread
 from threading import Event
+
+_BORNIBUS_ID = 1
+_R128_ID = 2
+_EYE_ID = 3
+
+_INTER_1_PIN = 26
+_INTER_2_PIN = 19
+_INTER_3_PIN = 13
+_INTER_4_PIN = 6
 
 class JetsonSwitch():
     def __init__(self, pin, attached_func):
@@ -38,15 +46,6 @@ class JetsonSwitch():
                 GPIO.setup(self.pin, GPIO.IN)
                 self.attached_func()
             sleep(0.05)
-
-_BORNIBUS_ID = 1
-_R128_ID = 2
-
-_INTER_1_PIN = 26
-_INTER_2_PIN = 19
-_INTER_3_PIN = 13
-_INTER_4_PIN = 6
-
 
 class ServerIHM():
     def __init__(self):
@@ -84,6 +83,9 @@ class ServerIHM():
 
         # Load default font.
         self.font = ImageFont.load_default()
+
+        # bind current message
+        self.current_message = self.init_message
 
         self._clear_picture()
 
@@ -125,41 +127,64 @@ class ServerIHM():
             GPU = int(GPU)/10
         return GPU
 
-    def show_init_message(self, clients):
+    def show_message(self, clients):
         if self.green_event.is_set():
             self._clear_picture()
             self.green_event.clear()
-            self.draw.text((self.x, self.top), "Green",  font=self.font, fill=255)
+            self.current_message = self.choose_color_message
 
         if self.yellow_event.is_set():
             self._clear_picture()
             self.yellow_event.clear()
-            self.draw.text((self.x, self.top), "Yellow",  font=self.font, fill=255)
 
         if self.blue_event.is_set():
             self._clear_picture()
             self.blue_event.clear()
-            self.draw.text((self.x, self.top), "Blue",  font=self.font, fill=255)
 
         if self.red_event.is_set():
             self._clear_picture()
             self.red_event.clear()
-            self.draw.text((self.x, self.top), "Red",  font=self.font, fill=255)
 
-        # self.draw.text((self.x, self.top), "wlan0: " + str(self._get_ip_address('wlan0')),  font=self.font, fill=255)
-
-        # if _BORNIBUS_ID in list(clients):
-        #     self.draw.text((self.x, self.top + 8), "Bornibus : Ok !",  font=self.font, fill=255)
-        # else:
-        #     self.draw.text((self.x, self.top + 8), "Bornibus : None",  font=self.font, fill=255)
-        # if _R128_ID in list(clients):
-        #     self.draw.text((self.x, self.top + 16), "128 Ok !",  font=self.font, fill=255)
-        # else:
-        #     self.draw.text((self.x, self.top + 16), "128 : None",  font=self.font, fill=255)
-
-        # self.draw.text((self.x, self.top + 24), "Press to start ...",  font=self.font, fill=255)
         try :
+            self.current_message()
             self.screen.image(self.image)
             self.screen.display()
         except IOError:
             print('screen error')
+
+    def init_message(self):
+        self.ip_message()
+        self.draw.text((self.x, self.top + 16), "Supervisor",  font=self.font, fill=255)
+        self.begin_message()
+
+    def client_status_message(self, clients):
+        if _BORNIBUS_ID in list(clients):
+            self.draw.text((self.x, self.top), "Bornibus : Ok !",  font=self.font, fill=255)
+        else:
+            self.draw.text((self.x, self.top), "Bornibus : None",  font=self.font, fill=255)
+        if _R128_ID in list(clients):
+            self.draw.text((self.x, self.top + 8), "R128 Ok !",  font=self.font, fill=255)
+        else:
+            self.draw.text((self.x, self.top + 8), "R128 : None",  font=self.font, fill=255)
+
+        if _EYE_ID in list(clients):
+            self.draw.text((self.x, self.top + 16), "EYE Ok !",  font=self.font, fill=255)
+        else:
+            self.draw.text((self.x, self.top + 16), "EYE : None",  font=self.font, fill=255)
+
+    def begin_message(self):
+        self.draw.text((self.x, self.top + 24), "Press GREEN to start ...",  font=self.font, fill=255)
+
+    def ip_message(self):
+        self.draw.text((self.x, self.top), "wlan0: " + str(self._get_ip_address('wlan0')),  font=self.font, fill=255)
+
+    def choice_message(self):
+        self.draw.text((self.x, self.top + 16), "GREEN : Save",  font=self.font, fill=255)
+        self.draw.text((self.x, self.top + 24), "RED   : Cancel",  font=self.font, fill=255)
+
+    def waiting_for_calibration_message(self):
+        self.draw.text((self.x, self.top), "Calibration...",  font=self.font, fill=255)
+
+    def choose_color_message(self):
+        self.draw.text((self.x, self.top), "Select Color...",  font=self.font, fill=255)
+        self.draw.text((self.x, self.top + 8), "YELLOW or BLUE",  font=self.font, fill=255)
