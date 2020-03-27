b5fbdb1e7a31358963e6f486f47ff058fd5f6f9e
Mathis Lécrivain <matlecr35@gmail.com>
2020-03-27 13:02:12 +0100

    work in progress...
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/listeners/position_listener.py
+++ raspberrypi/listeners/position_listener.py
@@ -8,12 +8,13 @@ from math import hypot
 from common.sync_flag_signal import Signal, Flag
 from common.tcptalks import NotConnectedError
 
+
 class PositionListener(Thread):
     BROTHER = 0
     OPPONENTA = 1
     OPPONENTB = 2
 
-    def __init__(self, brother_getter, opponents_getter, timestep=0.1, threshold=10):
+    def __init__(self, brother_getter, opponents_getter, timestep=0.5, threshold=10):
         Thread.__init__(self)
         self.daemon = True
 
@@ -21,18 +22,20 @@ class PositionListener(Thread):
         self.robots_list = [self.BROTHER, self.OPPONENTA, self.OPPONENTB]
 
         # Signals for each robots
-        self.__setattr__("signal"+str(self.BROTHER) , Signal())
-        self.__setattr__("signal"+str(self.OPPONENTA) , Signal())
-        self.__setattr__("signal"+str(self.OPPONENTB) , Signal())
+        self.__setattr__("signal"+str(self.BROTHER), Signal())
+        self.__setattr__("signal"+str(self.OPPONENTA), Signal())
+        self.__setattr__("signal"+str(self.OPPONENTB), Signal())
 
         # Brother and opponents getter
-        self.__setattr__("getter"+str(self.BROTHER) , brother_getter)
-        self.__setattr__("getter"+str(self.OPPONENTA) , lambda: opponents_getter()[0])
-        self.__setattr__("getter"+str(self.OPPONENTB) , lambda: opponents_getter()[1])
+        self.__setattr__("getter"+str(self.BROTHER), brother_getter)
+        self.__setattr__("getter"+str(self.OPPONENTA),
+                         lambda: opponents_getter()[0])
+        self.__setattr__("getter"+str(self.OPPONENTB),
+                         lambda: opponents_getter()[1])
 
-        self.__setattr__("position"+str(self.BROTHER) , (-1000, -1000))
-        self.__setattr__("position"+str(self.OPPONENTA) , (-1000, -1000))
-        self.__setattr__("position"+str(self.OPPONENTB) , (-1000, -1000))
+        self.__setattr__("position"+str(self.BROTHER), (-1000, -1000))
+        self.__setattr__("position"+str(self.OPPONENTA), (-1000, -1000))
+        self.__setattr__("position"+str(self.OPPONENTB), (-1000, -1000))
 
         # Timestep
         self.timestep = timestep
@@ -46,28 +49,35 @@ class PositionListener(Thread):
         # Position error
         self.error = 0
 
+        self.pos = (-1000, -1000)
+
         # Atomatically start
         self.start()
 
     def bind(self, idx, func):
         self.__setattr__("flag"+str(idx), Flag(func))
-        self.__getattribute__("flag"+str(idx)).bind(self.__getattribute__("signal"+str(idx)))
+        self.__getattribute__(
+            "flag"+str(idx)).bind(self.__getattribute__("signal"+str(idx)))
 
     def get_position(self, idx):
         return self.__getattribute__("position"+str(idx))
 
     def _handle_position(self, idx):
-        pos = self.__getattribute__("getter"+str(idx))()
+        try:
+            self.pos = self.__getattribute__("getter"+str(idx))()
+        except:
+            print('error')
 
-        x, y = pos[:2]
+        x, y = self.pos[:2]
         if (hypot(y - self.__getattribute__("position"+str(idx))[1], x - self.__getattribute__("position"+str(idx))[0]) + self.error) > self.threshold:
             self.__getattribute__("signal"+str(idx)).ping()
 
             self.error = 0
         else:
-            self.error += hypot(y - self.__getattribute__("position"+str(idx))[1], x - self.__getattribute__("position"+str(idx))[0])
+            self.error += hypot(y - self.__getattribute__("position"+str(idx))
+                                [1], x - self.__getattribute__("position"+str(idx))[0])
 
-        self.__setattr__("position"+str(idx), (x , y))
+        self.__setattr__("position"+str(idx), (x, y))
 
     def run(self):
         while not self.stop.is_set():
@@ -80,6 +90,8 @@ class PositionListener(Thread):
 
             sleep(self.timestep)
 
+
 if __name__ == "__main__":
 
-    p = PositionListener(lambda: (-1000, -1000), lambda: [(-2000, -1000),(-3000, -1000)])
+    p = PositionListener(lambda: (-1000, -1000),
+                         lambda: [(-2000, -1000), (-3000, -1000)])
--- raspberrypi/robots/bornibus/setup_bornibus.py
+++ raspberrypi/robots/bornibus/setup_bornibus.py
@@ -6,15 +6,15 @@ from setups.setup_logger import *
 from setups.setup_serialtalks import *
 from setups.setup_wheeledbase import *
 from setups.setup_roadmap import *
-from setups.setup_display import *
+#from setups.setup_display import *
 #from setups.setup_sensors import *
 from setups.setup_beacons import *
 
-geogebra , roadmap = init_roadmap(ROBOT_ID, BORNIBUS_ID)
+geogebra, roadmap = init_roadmap(ROBOT_ID, BORNIBUS_ID)
 
 
 def init_robot():
-    setup_logger(INFO, "Intialize robot !")
+    setup_logger(INFO, "Intialize !")
 
     setup_logger(INFO, "Ready !")
     pass
